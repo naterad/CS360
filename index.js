28ed7a87b681aa9f5c950cfa99ec26bd74710d29
Nathan Radmall <nathanradmall@gmail.com>
2015-11-14 19:25:09 -0700

    setting stuff up

---
+++ index.js
@@ -0,0 +1,9 @@
+// setup Express
+var app = require('./models/express.js');
+
+// start the server
+var server = app.listen(3000, function() {
+console.log("Started on port 3000");
+var host = server.address().address;
+var port = server.address().port;
+});
